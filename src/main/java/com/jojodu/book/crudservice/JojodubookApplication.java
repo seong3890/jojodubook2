43bb5c6ce2b44fa500070a7ea3e039008e8ecac7
seong3890 <audtjddl100@naver.com>
2022-12-23 20:04:54 +0900

    Initial commit

---
+++ src/main/java/com/jojodu/book/crudservice/JojodubookApplication.java
@@ -0,0 +1,13 @@
+package com.jojodu.book.crudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JojodubookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JojodubookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jojodu/book/crudservice/JojodubookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojodu.book.crudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JojodubookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
